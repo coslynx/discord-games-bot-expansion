fc009a4ec91e9e7e158fe5399fdedaf851c786a1
Spectra.codes by Drix10 <drixisgod@gmail.com>
2024-08-10 12:40:05 +0530

    generated file: `utils/logger.js`

---
+++ utils/logger.js
@@ -0,0 +1,23 @@
+const winston = require('winston');
+
+const { combine, timestamp, label, printf } = winston.format;
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: combine(
+    label({ label: 'Discord-Game-Bot' }),
+    timestamp(),
+    myFormat
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/combined.log' }),
+  ],
+});
+
+module.exports = logger;
